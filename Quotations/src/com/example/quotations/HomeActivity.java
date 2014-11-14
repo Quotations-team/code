5614f3fde1567c072745d4cc1f120bf78c571878
Saman <saman.missaghian@gmail.com>
2014-11-13 17:39:05 -0800

    Home listview-item layout created!

--- Quotations/src/com/example/quotations/HomeActivity.java
+++ Quotations/src/com/example/quotations/HomeActivity.java
@@ -2,8 +2,6 @@ package com.example.quotations;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.TreeSet;
 
 import com.example.quotations.R;
 import com.parse.FindCallback;
@@ -14,14 +12,12 @@ import com.parse.starter.*;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -34,93 +30,70 @@ import android.widget.Toast;
  */
 public class HomeActivity extends Activity
 {
-	List<QuotationTable> wholeQuotationTable, randomQuotationTable;
-	Set<Integer> tempUniqueTable;
-	int tempIndex;
+    ListView listView;
+    ListviewAdapter adapter;
+    ArrayList<Quote> quotes;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
-          super.onCreate(savedInstanceState);
-          setContentView(R.layout.activity_home);
+		super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-          ParseAnalytics.trackAppOpenedInBackground(getIntent());
-          init();
-          queryData();
+        ParseAnalytics.trackAppOpenedInBackground(getIntent());
+
+        //loadNewQuotes();
+        quotes= new ArrayList<Quote>();
+        quotes.add(new Quote(100, "SS", "DD"));
+
+        updateListView();
       }
+	   //this query will be change later by only query here, only once and save all data in cell phone.
+    //when we need to access the data again, we access the local stored data.
+    public void loadNewQuotes() {
+        //retrieve list of data from parse.com server
+        //can create ParseQuery object or use getQuery()
+        //"QuotationTable" is our table
+        //ParseQuery<QuotationTable> query = new ParseQuery<QuotationTable>("QuotationTable");
+        ParseQuery<QuotationTable> query = ParseQuery.getQuery("QuotationTable");
 
-	public void init()
-	{
-		wholeQuotationTable = new ArrayList<QuotationTable>();
-		randomQuotationTable = new ArrayList<QuotationTable>();
-		tempUniqueTable = new TreeSet<Integer>();
-		tempIndex = 0;
-	}
+        //order the display, by sorting a field
+        query.orderByAscending("ID");
 
-	//this query will be change later by only query here, only once and save all data in cell phone.
-	//when we need to access the data again, we access the local stored data.
-	public void queryData()
-	{
-		//retrieve list of data from parse.com server
-		//can create ParseQuery object or use getQuery()
-		//"QuotationTable" is our table
-		//ParseQuery<QuotationTable> query = new ParseQuery<QuotationTable>("QuotationTable");
-		ParseQuery<QuotationTable> query = ParseQuery.getQuery("QuotationTable");
-		//order the display, by sorting a field
-		query.orderByAscending("ID");
-		//findInBackground() fetch ParseObject QuotationTable from database
-		//it has its own thread
-		query.findInBackground(new FindCallback<QuotationTable>()
-		{
-			//when fetch is done
-			//List<QuotationTable> quotations will be our ParseObject from the parse.com database, our table
-			@Override
-			public void done(List<QuotationTable> quotations, ParseException e)
-			{
-				if (e != null)
-				{
-					Toast.makeText(getBaseContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
-				}
-				else
-				{
-					for(QuotationTable quotation : quotations)
-					{
-						QuotationTable newQuote = new QuotationTable();
-						newQuote.setID(quotation.getID());
-						newQuote.setCategory(quotation.getCategory());
-						newQuote.setQuote(quotation.getQuote());
-						wholeQuotationTable.add(newQuote);
-					}
-				}
-				//need to call it here, wait for fetch data,
-				//if later on stored data on phone, can call it somewhere else
-				generateRandomQuote(4);
-			}
-		});
-	}
+        //findInBackground() fetch ParseObject QuotationTable from database
+        //it has its own thread
+        query.findInBackground(new FindCallback<QuotationTable>() {
+            //when fetch is done
+            //List<QuotationTable> quotations will be our ParseObject from the parse.com database, our table
+            @Override
+            public void done(List<QuotationTable> quotations, ParseException e) {
+                if (e != null) {
+                    Toast.makeText(getBaseContext(), "Error: " + e, Toast.LENGTH_SHORT).show();
+                } else {
+                    for (QuotationTable quotation : quotations) {
+                        quotes.add(new Quote(quotation.getID(), quotation.getCategory(), quotation.getQuote()));
+                    }
+                }
+                updateListView();
+            }
+        });
+    }
 
-	public void generateRandomQuote(int numOfRanQuote)
-	{
-		while(tempUniqueTable.size() < numOfRanQuote)
-		{
-			tempIndex = getRandomValue(0, wholeQuotationTable.size()-1);
-			tempUniqueTable.add(tempIndex);
-		}
-		for(int i : tempUniqueTable)
-		{
-			randomQuotationTable.add(wholeQuotationTable.get(i));
-		}
-		
-		ListView listView = (ListView) findViewById(R.id.lvRandomQuote);
-		ArrayAdapter<QuotationTable> arrayAdapter = new ArrayAdapter<QuotationTable>(getBaseContext(), android.R.layout.simple_list_item_1, randomQuotationTable);
-		listView.setAdapter(arrayAdapter);
-		listView.setBackgroundColor(Color.LTGRAY);
-	}
+    public  void updateListView()
+    {
+        adapter = new ListviewAdapter(this, quotes);
 
-	public int getRandomValue(int minimumRange, int maximumRange)
-	{
-		return((int)((maximumRange-minimumRange+1)*Math.random() + minimumRange));
-	}
+        listView = (ListView) findViewById(R.id.lvRandomQuote);
+        listView.setAdapter(adapter);
+
+        // Click event for single list row
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+    }
 	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item)
@@ -156,17 +129,4 @@ public class HomeActivity extends Activity
 	    return true;
 	}
 	
-	public void onHomePageToSrchPage(View v)
-	{
-		Toast.makeText(this, "Clicked Button launch SearchActivity", Toast.LENGTH_SHORT).show();
-		Intent iBtnHomePageToSrchPage = new Intent(getBaseContext(), SearchActivity.class);
-		startActivity(iBtnHomePageToSrchPage);
-	}
-	
-	public void onHomePageToCatPage(View v)
-	{
-		Toast.makeText(this, "Clicked Button launch CategoryActivity", Toast.LENGTH_SHORT).show();
-		Intent iBtnHomePageToCatPage = new Intent(getBaseContext(), CategoryActivity.class);
-		startActivity(iBtnHomePageToCatPage);
-	}
 }
